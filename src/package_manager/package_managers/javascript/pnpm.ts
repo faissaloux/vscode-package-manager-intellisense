44936dba5fb8c620d9b0cabd4e8864509be3fa53
faissaloux <fwahabali@gmail.com>
2026-02-06 21:48:50 +0100

    show latest version for pnpm package manager

--- src/package_manager/package_managers/javascript/pnpm.ts
+++ src/package_manager/package_managers/javascript/pnpm.ts
@@ -1,8 +1,10 @@
 import { JavascriptPackageManagerInterface } from "../../../interfaces/javascript_package_manager";
+import { outdated } from "../../../types/types";
 import JavascriptPackageManager from './javascript_package_manager';
 
 export class Pnpm extends JavascriptPackageManager implements JavascriptPackageManagerInterface {
     protected readonly locks = ['pnpm-lock.yaml'];
+    protected readonly outdatedPackagesCommand: string = 'pnpm outdated --json';
     protected readonly startsWith: {[version: string | number]: string} = {
         /* eslint-disable @typescript-eslint/naming-convention */
         '5.3': '/packageName/',
@@ -13,5 +15,19 @@ export class Pnpm extends JavascriptPackageManager implements JavascriptPackageM
 
     getName(): string {
         return 'pnpm';
+    }
+
+    getLatestVersions(): outdated[] {
+        const outdatedPackages: string = this.getOutdatedPackages();
+
+        return Object.entries(JSON.parse(outdatedPackages))
+            // @ts-ignore
+            .map(([pkgName, pkg]: [string, {current: string, latest: string}]) => {
+                return {
+                    package: pkgName.replace('(dev)', '').trim(),
+                    version: pkg.current,
+                    latestVersion: pkg.latest,
+                };
+            });
     }
 }
