dea3d1c6d112eee0d84c9a31dd292f120e8cb75d
faissaloux <fwahabali@gmail.com>
2026-02-11 00:27:16 +0100

    show latest versions for rust dependencies

--- src/package_manager/package_managers/rust.ts
+++ src/package_manager/package_managers/rust.ts
@@ -1,11 +1,13 @@
+import * as vscode from 'vscode';
 import * as toml from '@iarna/toml';
 import { PackageManager } from '../../interfaces/package_manager';
 import { LanguagePackageManager } from '../language_package_manager';
 import { Parser } from '../../parser/parser';
-import { InstalledPackage, Language } from '../../types/types';
+import { InstalledPackage, Language, outdated } from '../../types/types';
 
 export class Rust extends LanguagePackageManager implements PackageManager {
     protected name: Language = 'rust';
+    protected readonly outdatedPackagesCommand: string = 'cargo outdated';
     protected readonly packagePattern: string = 'placeholder ';
 
     async getInstalled(packageName: string, line: string): Promise<InstalledPackage> {
@@ -25,6 +27,29 @@ export class Rust extends LanguagePackageManager implements PackageManager {
 
     override getLockPath(): string {
         return 'Cargo.lock';
+    }
+
+    getLatestVersions(): outdated[] {
+        const outdatedPackages = this.getOutdatedPackages();
+        
+        if (outdatedPackages === '') {
+            vscode.window.showWarningMessage(
+                'You need to install cargo-outdated to be able to see the latest versions of your dependencies.\n' +
+                'Run: cargo install cargo-outdated',
+            );
+        }
+
+        return outdatedPackages.split('\n')
+            .filter(line => /\d/.test(line))
+            .map(line => {
+                const lineParts = line.split('  ').filter(part => part !== '').map(part => part.trim());
+
+                return {
+                    package: lineParts[0],
+                    version: lineParts[1],
+                    latestVersion: lineParts[3],
+                };
+            });
     }
 
     getPackagesNames(content: string): Set<string> {
